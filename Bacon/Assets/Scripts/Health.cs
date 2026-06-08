a5ee0135e230758ca3a13cdeb8482fe4ebb74cbe
tihico <timothy.hillenaar@gmail.com>
2026-06-08 13:56:55 +0200

    Health system aangemaakt

---
+++ Bacon/Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int Healthy = 3;
+    public TextMeshProUGUI health;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            health.text = "Health: " + Healthy.ToString();
+            Healthy = Healthy - 1;
+        }
+    }
+}
+
