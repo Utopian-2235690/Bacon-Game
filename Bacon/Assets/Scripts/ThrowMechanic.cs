de1840274a0c5bf5fd612c61054c774594b1800e
AirplaneEnjoyer <2239378@talnet.nl>
2026-06-19 09:09:42 +0200

    ThrowMechanic
    Added a Throw Mechanic script

--- Bacon/Assets/Scripts/ThrowMechanic.cs
+++ Bacon/Assets/Scripts/ThrowMechanic.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
 public class ThrowMechanic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public SpringJoint2D spring;
+
+
+    void Awake()
     {
-        
+
+        spring = this.gameObject.GetComponent<SpringJoint2D>(); //"spring" is the SpringJoint2D component that I added to my object
+
+        spring.connectedAnchor = gameObject.transform.position;//i want the anchor position to start at the object's position
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnMouseDown()
     {
-        
+
+        spring.enabled = true;//I'm reactivating the SpringJoint2D component each time I'm clicking on my object becouse I'm disabling it after I'm releasing the mouse click so it will fly in the direction i was moving my mouse
+
     }
+
+
+    void OnMouseDrag()
+    {
+
+        if (spring.enabled == true)
+        {
+
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//getting cursor position
+
+            spring.connectedAnchor = cursorPosition;//the anchor get's cursor's position
+
+
+        }
+    }
+
+
+    void OnMouseUp()
+    {
+
+        spring.enabled = false;//disabling the spring component
+
+    }
+
 }
